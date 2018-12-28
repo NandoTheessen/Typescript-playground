de56ddf155442b16af296dbcdacaa7395da1f371
NandoTheessen <nando.theessen@gmail.com>
2018-12-28 04:20:28 +0100

    Add types for express, morgan - Define script for nodemon - remove tsc from build command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import express from 'express';
 import add from './add';
+import morgan from 'morgan';
 
+export const server = express();
 export const a: number = 5;
 export const b: number = 15;
+
+server.use(morgan('dev'));
+server.get('/', (req, res) => {
+  res.send({ msg: 'booooojaaa!' });
+});
+server.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+server.listen(3000, () => console.log('Server is listening!'));
 
 console.log('Hello World!', add(a, b));
